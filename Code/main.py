bf9f19c549ad26077fa2a0ceac6dae8303cdcf9f
al-Capun <antoninfriberg@gmail.com>
2020-03-22 11:59:00 +0100

    python routes für  feed & upload eingebunden, html upload seite test datei erstellt, json test file erstellt, flask in html integriert.

--- Code/main.py
+++ Code/main.py
@@ -1,20 +1,51 @@
 from flask import Flask
-from flask import request
 from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
+
+import json
+
+app = Flask("WebApp")
 
 
-"""
+def load_feed(path):
+    with open(path, "r", encoding="utf-8") as file:
+        result = json.load(file)
+        return result
 
-app = Flask("Demo")
+
+def save_files(path, data):
+    with open(path, "w", encoding="utf-8") as file:
+        json.dump(data, file)
 
 
-@app.route("/hello/", methods=['GET', 'POST'])
-def hallo():
-    if request.method == 'POST':
-        ziel_person = request.form['vorname']
-        rueckgabe_string = "Hello " + ziel_person + "!"
-        return rueckgabe_string
+# Feed laden
+@app.route('/')
+@app.route('/feed/')
+def feed():
+    files = load_feed('./static/uploaded-files.json')
+    return render_template("feed.html", files=files)
 
-    return render_template("index.html")
 
-"""
+@app.route('/file_upload/', methods=["GET", "POST"])
+def file_upload():
+    files = load_feed('./static/uploaded-files.json')
+    if request.method == "POST":
+        name = request.form.get("name")
+        beschreibung = request.form.get("beschreibung")
+        new_file = {}
+        new_file["name"] = name
+        new_file["beschreibung"] = beschreibung
+        new_file["pfad"] = ""
+
+        files.append(new_file)
+        save_files('./static/uploaded-files.json', files)
+
+        return redirect(url_for("feed"))
+
+    return render_template("file_upload.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
